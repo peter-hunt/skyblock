5a96aa74eb47b8976e1d77b600bc07f224a1f32b
peter-hunt <huangtnhao@outlook.com>
2022-01-29 09:23:28 +0800

    Removed windows support

--- skyblock/install.py
+++ skyblock/install.py
@@ -2,7 +2,6 @@ from os import mkdir
 from os.path import join
 from pathlib import Path
 from subprocess import run
-from sys import platform
 
 from .path import is_data, join_path
 
@@ -25,10 +24,7 @@ def install_data(*, is_update=False):
         doing_str, done_str = 'Fixing', 'Fixed'
     if is_update or not is_data():
         if Path(data_dir).is_dir():
-            if platform == 'win32':
-                run(['rm', data_dir, '-Recurse', '-Force'])
-            else:
-                run(['rm', '-rf', data_dir])
+            run(['rm', '-rf', data_dir])
         mkdir(data_dir)
         print(f'\x1b[0;38;2;85;255;85m{doing_str} Assets...\x1b[0m')
         run(['git', 'clone', '-q',
