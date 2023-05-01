61cec8cbf487755df19c124a979c1a1f5a663ba3
TreesOnTop <85085371+TreesOnTop@users.noreply.github.com>
2023-05-01 13:57:18 -0400

    since you've stopped working on the project change the data repo to mine

--- build/lib/skyblock/install.py
+++ build/lib/skyblock/install.py
@@ -30,7 +30,7 @@ def install_data(*, is_update=False):
         mkdir(data_dir)
         print(f'\x1b[0;38;2;85;255;85m{doing_str} Assets...\x1b[0m')
         run(['git', 'clone', '-q',
-             'https://github.com/peter-hunt/skyblock-data', data_dir])
+             'https://github.com/TreesOnTop/skyblock-data', data_dir])
         if Path(data_dir).is_dir():
             print(f'\x1b[0;38;2;85;255;85mAssets {done_str}!\x1b[0m')
 
